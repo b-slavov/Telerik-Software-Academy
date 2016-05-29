d13721007bbb2b2545c93681dfa284430783a793
b-slavov <boyan.slavoff@gmail.com>
2016-05-29 13:43:20 +0300

    Added 3rd task!

---
+++ 02.C# Part 2/Exam-Preparation-3rd-Tasks/LoverOf3-V2/LoverOf3.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+
+class LoverOf3
+{
+    static int[,] FillTheMatrix(int rows, int cols)
+    {
+        int[,] matrix = new int[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i, j] = 3 * (rows - i - 1 + j);
+            }
+        }
+
+        return matrix;
+    }
+
+    static void Main()
+    {
+        int[] matrixSize = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int rows = matrixSize[0];
+        int cols = matrixSize[1];
+
+        int[,] matrix = FillTheMatrix(rows, cols);
+
+        int row = rows - 1;
+        int col = 0;
+        int result = 0;
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            string direction = input[0];
+            int moves = int.Parse(input[1]);
+
+            switch (direction)
+            {
+                case "UR": case "RU":
+                    for (int j = 0; j < moves - 1; j++)
+                    {
+                        if (row == 0 || col == cols - 1)
+                        {
+                            break;
+                        }
+
+                        row--;
+                        col++;
+                        result += matrix[row, col];
+                        matrix[row, col] = 0;
+                    }
+                    break;
+
+                case "UL": case "LU":
+                    for (int j = 0; j < moves - 1; j++)
+                    {
+                        if (row == 0 || col == 0)
+                        {
+                            break;
+                        }
+
+                        row--;
+                        col--;
+                        result += matrix[row, col];
+                        matrix[row, col] = 0;
+                    }
+                    break;
+
+                case "DR": case "RD":
+                    for (int j = 0; j < moves - 1; j++)
+                    {
+                        if (row == rows - 1 || col == cols - 1)
+                        {
+                            break;
+                        }
+
+                        row++;
+                        col++;
+                        result += matrix[row, col];
+                        matrix[row, col] = 0;
+                    }
+                    break;
+
+                case "DL": case "LD":
+                    for (int j = 0; j < moves - 1; j++)
+                    {
+                        if (row == rows - 1 || col == 0)
+                        {
+                            break;
+                        }
+
+                        row++;
+                        col--;
+                        result += matrix[row, col];
+                        matrix[row, col] = 0;
+                    }
+                    break;
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
---
+++ 02.C# Part 2/Exam-Preparation-3rd-Tasks/LoverOf3/LoverOf3.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+
+class LoverOf3
+{
+    static void Main()
+    {
+        int[] matrixSize = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int maxRows = matrixSize[0];
+        int maxCols = matrixSize[1];
+        int n = int.Parse(Console.ReadLine());
+
+        int rows = 0;
+        int cols = 0;
+        int result = 0;
+        bool[,] visited = new bool[matrixSize[0], matrixSize[1]];
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            string direction = input[0];
+            int moves = int.Parse(input[1]);
+
+            if (direction == "UR" || direction == "RU" && moves > 1)
+            {
+                for (int j = 0; j < moves - 1; j++)
+                {
+                    if (rows == maxRows - 1 || cols == maxCols - 1)
+                    {
+                        break;
+                    }
+
+                    rows++;
+                    cols++;
+                    if (!visited[maxRows - rows - 1, cols])
+                    {
+                        result += 3 * (rows + cols);
+                        visited[maxRows - rows - 1, cols] = true;
+                    }
+                }
+            }
+
+            if (direction == "UL" || direction == "LU" && moves > 1)
+            {
+                for (int j = 0; j < moves - 1; j++)
+                {
+                    if (rows == maxRows - 1 || cols == 0)
+                    {
+                        break;
+                    }
+
+                    rows++;
+                    cols--;
+                    if (!visited[maxRows - rows - 1, cols])
+                    {
+                        result += 3 * (rows + cols);
+                        visited[maxRows - rows - 1, cols] = true;
+                    }
+                }
+            }
+
+            if (direction == "DR" || direction == "RD" && moves > 1)
+            {
+                for (int j = 0; j < moves - 1; j++)
+                {
+                    if (rows == 0 || cols == maxCols - 1)
+                    {
+                        break;
+                    }
+
+                    rows--;
+                    cols++;
+                    if (!visited[maxRows - rows - 1, cols])
+                    {
+                        result += 3 * (rows + cols);
+                        visited[maxRows - rows - 1, cols] = true;
+                    }
+                }
+            }
+
+            if (direction == "DL" || direction == "LD" && moves > 1)
+            {
+                for (int j = 0; j < moves - 1; j++)
+                {
+                    if (rows == 0 || cols == 0)
+                    {
+                        break;
+                    }
+
+                    rows--;
+                    cols--;
+                    if (!visited[maxRows - rows - 1, cols])
+                    {
+                        result += 3 * (rows + cols);
+                        visited[maxRows - rows - 1, cols] = true;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
