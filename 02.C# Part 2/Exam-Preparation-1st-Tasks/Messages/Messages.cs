62df9b6f2a77fc2905af50b735c6d7c0b2b8e8d2
b-slavov <boyan.slavoff@gmail.com>
2016-05-31 22:59:10 +0300

    Solution updated!

--- 02.C# Part 2/Exam-Preparation-1st-Tasks/Messages/Messages.cs
+++ 02.C# Part 2/Exam-Preparation-1st-Tasks/Messages/Messages.cs
@@ -3,30 +3,6 @@ using System.Numerics;
 
 class Messages
 {
-    static void Main()
-    {
-        string a = Console.ReadLine();
-        string operation = Console.ReadLine();
-        string b = Console.ReadLine();
-
-        BigInteger numberA = GeorgeTheGreatToDecimal(a);
-        BigInteger numberB = GeorgeTheGreatToDecimal(b);
-
-        BigInteger result = 0;
-        if (operation == "+")
-        {
-            result = numberA + numberB;
-        }
-        else
-        {
-            result = numberA - numberB;
-        }
-
-        string output = DecimalToGeorgeTheGreat(result);
-
-        Console.WriteLine(output);
-    }
-
     static BigInteger GeorgeTheGreatToDecimal(string word)
     {
         string[] alphabet = new string[] { "cad", "xoz", "nop", "cyk", "min", "mar", "kon", "iva", "ogi", "yan" };
@@ -57,5 +33,29 @@ class Messages
         } while (dec > 0);
 
         return result;
+    }
+
+    static void Main()
+    {
+        string a = Console.ReadLine();
+        string operation = Console.ReadLine();
+        string b = Console.ReadLine();
+
+        BigInteger numberA = GeorgeTheGreatToDecimal(a);
+        BigInteger numberB = GeorgeTheGreatToDecimal(b);
+
+        BigInteger result = 0;
+        if (operation == "+")
+        {
+            result = numberA + numberB;
+        }
+        else
+        {
+            result = numberA - numberB;
+        }
+
+        string output = DecimalToGeorgeTheGreat(result);
+
+        Console.WriteLine(output);
     }
 }
