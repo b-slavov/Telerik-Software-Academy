7eef9ccb647c22127da806028f4e00caf128f2cc
b-slavov <boyan.slavoff@gmail.com>
2016-06-30 17:20:24 +0300

    Solution updated!

--- 06.JavaScript Fundamentals/09.Strings/solution-2016.js
+++ 06.JavaScript Fundamentals/09.Strings/solution-2016.js
@@ -54,13 +54,43 @@ function solve(args) {
 // Problem 4. Parse tags
 
 function solve(args) {
-    var text = args[0];
+    let text = args[0],
+        inTag = false,
+        currentTag = '',
+        tags = [],
+        result = [];
 
-    text = text.replace(/<upcase>(.*?)<\/upcase>/g, function (a, l) { return l.toUpperCase(); });
-    text = text.replace(/<lowcase>(.*?)<\/lowcase>/g, function (a, l) { return l.toLowerCase(); });
-    text = text.replace(/<orgcase>(.*?)<\/orgcase>/g, '$1');
+    for (var i = 0; i < text.length; i++) {
+        if (text[i] === '<') {
+            inTag = true;
+        } else if (text[i] === '>' && inTag) {
+            inTag = false;
+            currentTag += text[i];
+            tags.push(currentTag);
+            currentTag = '';
+        } else if (!inTag) {
+            if (tags.length === 0) {
+                result.push(text[i]);
+            } else if (tags[tags.length - 1].indexOf('upcase') !== -1) {
+                result.push(text[i].toUpperCase());
+            } else if (tags[tags.length - 1].indexOf('lowcase') !== -1) {
+                result.push(text[i].toLowerCase());
+            } else {
+                result.push(text[i]);
+            }
+        }
 
-    console.log(text);
+        if (inTag || text[i] === '>') {
+            currentTag += text[i];
+        }
+
+        if (tags.length !== 0 && tags[tags.length - 1].indexOf('</') !== -1) {
+            tags.pop();
+            tags.pop();
+        }
+    }
+
+    console.log(result.join(''));
 }
 
 // Problem 5. nbsp
