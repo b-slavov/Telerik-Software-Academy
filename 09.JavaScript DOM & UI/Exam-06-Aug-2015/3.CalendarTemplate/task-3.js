6745e0a743d884f2022e1d2cf7b075677e96cac0
b-slavov <boyan.slavoff@gmail.com>
2016-08-16 00:12:17 +0300

    fix

--- 09.JavaScript DOM & UI/Exam-06-Aug-2015/3.CalendarTemplate/task-3.js
+++ 09.JavaScript DOM & UI/Exam-06-Aug-2015/3.CalendarTemplate/task-3.js
@@ -1,6 +1,28 @@
 function solve() {
     return function (selector) {
-        var template = '';
+        var template =
+            '<div class="events-calendar">' +
+                '<h2 class="header">Appointments for <span class="month">{{month}}</span> <span class="year">{{year}}</span></h2>' +
+                '{{#days}}' +
+                '<div class="col-date">' +
+                    '<div class="date">{{day}}</div>' +
+                    '<div class="events">' +
+                        '{{#events}}' +
+                            '{{#if title}}' +
+                            '<div class="event {{importance}}" title="{{comment}}">' +
+                                '<div class="title">{{title}}</div>' +
+                                '<span class="time">at: {{time}}</span>' +
+                            '</div>' +
+                            '{{else}}' +
+                            '<div class="event {{importance}}">' +
+                                '<div class="title">Free slot</div>' +
+                            '</div>' +
+                            '{{/if}}' +
+                        '{{/events}}' +
+                    '</div>' +
+                '</div>' +
+                '{{/days}}' +
+            '</div>';
         document.getElementById(selector).innerHTML = template;
     };
 }
